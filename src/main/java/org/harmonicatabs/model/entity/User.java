1d9584987d3002e7439804de5e3897fa413119b4
konstantin <konstantin.pl.andreev@gmail.com>
2024-07-14 14:28:07 +0300

    Test commit

--- src/main/java/org/harmonicatabs/model/entity/User.java
+++ src/main/java/org/harmonicatabs/model/entity/User.java
@@ -1,4 +1,11 @@
 package org.harmonicatabs.model.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
 }
